f4649247fde56955d361ec9a9f97a498d6cfd191
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-10-01 16:24:48 +0530

    Ansible

--- expense-v2/main.tf
+++ expense-v2/main.tf
@@ -1,14 +1,9 @@
-module "frontend" {
+module "expense" {
+   count = 3
    source = "./app-create"
-   component = "frontend"
+   component = var.expense[count.index]
 }
 
-module "backend" {
-   source = "./app-create"
-   component = "backend"
-}
-
-module "mysql" {
-   source = "./app-create"
-   component = "mysql"
+variable "expense" {
+   default = [ "frontend", "backend", "mysql" ]
 }
