c7619b9097672b880b7c0ad30e28775e225350d1
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-11-21 21:55:01 +0530

    Ansible

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -4,3 +4,25 @@ resource "null_resource" "null" {
       command = "echo index No - ${count.index}"
    }
 }
+
+variable "fruits" {
+   default = {
+      apple = {
+         name  = "Apple"
+         count = 100
+      }
+
+      banana = {
+         name  = "Banana"
+         count = 20
+      }
+
+   }
+}
+
+resource "null_resource" "test1" {
+   for_each = var.fruits
+   provisioner "local-exec" {
+      command = "echo Fruit Name - ${each.value["name"]}, Fruit Count - ${each.value["count"]}"
+   }
+}
